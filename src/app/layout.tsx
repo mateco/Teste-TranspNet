8bd9d4b79d1e6725c2f62b62ec6d92d593b7d1a2
mateco <riosmachadomatheus@gmail.com>
2025-06-01 19:00:43 -0300

    hot-fix-footer-desktop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,9 +29,9 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} bg-black text-white antialiased flex flex-col min-h-screen`}>
         <Header />
 
-        <main className="flex-1 w-full max-w-7xl mx-auto px-4 md:px-16 pt-64 md:pt-48 pb-20">
-          {children}
-        </main>
+          <main className="flex-1 w-full max-w-7xl mx-auto px-4 md:px-16 pt-64 md:pt-48 pb-20 md:pb-0">
+            {children}
+          </main>
 
         <Footer />
       </body>
